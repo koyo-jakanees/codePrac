c76d53cb588aaa4325dfa68279b2d01567ae6037
koyo-jakanees <dakoyo094@gmail.com>
2019-02-11 15:11:58 +0300

    create and edit Monday js prac

---
+++ javascript/jsMondays.js
@@ -0,0 +1,14 @@
+/*functions to try out in-built method 
+toExponential*/
+
+var firstNumber = 9238.479;
+console.log(firstNumber.toExponential());
+
+var numTwo = 98370923874.32;
+console.log(numTwo.toExponential());
+
+/* round up to the nearest decimal or 
+whole number */
+
+var deciNum = 324.64322;
+console.log(deciNum.toFixed(2));
