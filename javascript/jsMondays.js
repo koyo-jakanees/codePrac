8d48ec72833825bc80fe9774a1f1131036bf5839
koyo-jakanees <dakoyo094@gmail.com>
2019-02-11 16:01:49 +0300

    string methods intro

--- javascript/jsMondays.js
+++ javascript/jsMondays.js
@@ -12,3 +12,37 @@ whole number */
 
 var deciNum = 324.64322;
 console.log(deciNum.toFixed(2));
+
+//String Methods.
+// to uppercase letters
+
+console.log("I love Python,julia and Scala".toUpperCase());
+
+//to lowercase
+
+var statement = "LOREM IPSUM";
+console.log(statement.toLowerCase());
+
+//string concat
+
+console.log("Pine".concat("Avocado"));
+
+console.log("Example" + "Here");
+
+//chaining string methods
+console.log("I am ".concat("so excited").toUpperCase());
+
+
+//String indexing. Zero based indices
+
+console.log("Jakanees".charAt(3));
+console.log("Jakanees".charAt(0));
+console.log("Jakanees".charAt(8));
+
+var stringSearch = "please show me where the 'show' is located";
+console.log(stringSearch.search("show"));
+
+//string slicing
+var fruitList = "Papaya, Lemon, Avocado";
+console.log(fruitList.slice(8, 13));
+
