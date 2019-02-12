79ef90458fad76cb3f832ce7e849dc6426a0c4cf
koyo-jakanees <dakoyo094@gmail.com>
2019-02-12 12:09:06 +0300

    js fiddle prac

---
+++ javascript/jsTuesday.js
@@ -0,0 +1,16 @@
+var saySomething = function(whatToSay) {
+  alert(whatToSay);
+  };
+  
+  var add = function(number1, number2) {
+  return number1 + number2;
+  };
+  
+  /*
+  saySomething("hi"); 
+  alert(add(3, 5));   
+  */
+  var result = add(91, 78);
+  var outputText = "The sum is " + result + ".":
+  
+  saySomething(outputText);
